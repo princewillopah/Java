92df1ff1a4d550803230bbb91700bff219706fc2
ptincewillopah <proincewillopah@gmail.com>
2024-03-07 08:36:05 +0000

    new push

--- _1_class/_7_Interface/_1_Animal.java
+++ _1_class/_7_Interface/_1_Animal.java
@@ -1,5 +1,5 @@
 package _7_Interface;
-public interface Animal {
+public interface _1_Animal {
     public void animalSound(); // interface method (does not have a body)
     public void sleep(); // interface method (does not have a body)
     public void run(); // interface method (does not have a body)
---
+++ _1_class/_7_Interface/_1_Pig.java
@@ -0,0 +1,17 @@
+import _7_Interface._1_Animal;
+  // Pig "implements" the Animal interface
+  public class _1_Pig implements _1_Animal {
+    public void animalSound() {
+      // The body of animalSound() is provided here
+      System.out.println("The pig says: wee wee");
+    }
+    public void sleep() {
+      // The body of sleep() is provided here
+      System.out.println("Zzz");
+    }
+    public void run() {
+        // The body of sleep() is provided here
+        System.out.println("zoooooom!");
+      }
+
+  }
